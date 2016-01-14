397bd8dbae285dc5e7061a686ed07404f5f569f4
muralikrishna <muralikrishna@sparshcom.net>
2016-01-14 16:12:35 +0530

    check the dev
    check the dev

--- wp-content/themes/twentyfifteen/header.php
+++ wp-content/themes/twentyfifteen/header.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying the header
+ * The template for displaying the header displaying
  *
  * Displays all of the head element and everything up until the "site-content" div.
  *
